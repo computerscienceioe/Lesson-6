55d42eb4b15048d99539bfe4be27ee9e615e19b7
MrBenjaminCounter <77814190+MrBenjaminCounter@users.noreply.github.com>
2021-01-24 21:41:37 +0000

    Create Challenge 1.py

---
+++ Challenge 1.py
@@ -0,0 +1,6 @@
+
+age = float(input("Enter your age: "))
+if age >= 17:
+  print("You are eligible to apply for an Irish Driving Licence!")
+else:
+  print("Unfortunately, you are not eligible \nto apply for an Irish Driving Licence.")
